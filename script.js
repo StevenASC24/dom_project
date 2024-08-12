2c2374d5e579e088d010b1fed786e879779280f7
StevenASC24 <Stevendlt9@gmail.com>
2024-08-12 11:05:09 -0400

    first commit

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-// Add your code to this file
+let heading = document.querySelector("h1");
+console.log(heading);
+
+heading.style.textDecoration = "underline";
+
+let heading2 = document.querySelector("h2");
+console.log(heading2);
+
+heading2.style.textAlign = "center";
+heading2.style.innerHTML += " Here are  <br><br><br>";
+
+let img = document.getElementById("#starters_image_div");
+
+for (i = 0; i < img.length; i++) {
+img[i].style.border = ("10px dotted orange")
+}
